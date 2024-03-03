df8f7fc18a8af2f73995f902537ea4af28333924
DAIKI <daiki.evilone@gmail.com>
2024-03-03 23:38:22 +0900

    Add package of TPS camera

---
+++ src/ActionGameCore/Assets/TPSCameraControls/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [1.0.0] - 2024-03-03
+### Added
+- Add camera rotation and wall check.
---
+++ src/ActionGameCore/Assets/TPSCameraControls/Scripts/Runtime/TPSCameraController.cs
@@ -0,0 +1,93 @@
+#if SUPPORT_INPUTSYSTEM
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FPSCameraControls
+{
+    public class TPSCameraController : MonoBehaviour
+    {
+        public enum OffsetRotationType
+        {
+            FullRotation = 0,
+            HorizontalRotation = 1,
+        }
+
+        [SerializeField]
+        private InputActionAsset _inputActionAsset = null;
+
+        [SerializeField]
+        public Transform Target = null;
+
+        [SerializeField]
+        public Vector3 Offset = Vector3.zero;
+
+        [SerializeField]
+        public OffsetRotationType OffsetRotation = OffsetRotationType.HorizontalRotation;
+
+        [SerializeField]
+        public float Distance = 5.0f;
+
+        [SerializeField]
+        public float Sensitivity = 0.1f;
+
+        [SerializeField]
+        public float AngleMin = -90.0f;
+
+        [SerializeField]
+        public float AngleMax = 90.0f;
+
+        [SerializeField]
+        public bool IsCheckWall = true;
+
+        [SerializeField]
+        public LayerMask WallLayerMask = ~0;
+
+        [SerializeField]
+        private bool _lockAndHideCursor = true;
+
+        private InputAction _lookAction;
+        private Vector3 _lookAngles;
+
+        private void Start()
+        {
+            _inputActionAsset.Enable();
+            var map = _inputActionAsset.FindActionMap("Player");
+            _lookAction = map.FindAction("Look");
+
+            if (_lockAndHideCursor)
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (Target == null)
+            {
+                return;
+            }
+
+            var look = _lookAction.ReadValue<Vector2>();
+            _lookAngles.x = Mathf.Clamp(_lookAngles.x - look.y * Sensitivity, -AngleMax, -AngleMin); // Look up and down
+            _lookAngles.y = (_lookAngles.y + look.x * Sensitivity) % 360; // Look left and right
+            var rotation = Quaternion.Euler(_lookAngles);
+
+            var offsetRotation = OffsetRotation == OffsetRotationType.FullRotation ? rotation : Quaternion.AngleAxis(_lookAngles.y, Vector3.up);
+            var pivot = Target.position + offsetRotation * Offset;
+
+            var distance = Distance;
+            if (IsCheckWall)
+            {
+                var ray = new Ray(pivot, rotation * Vector3.back);
+                if (Physics.Raycast(ray, out var hit, Distance, WallLayerMask))
+                {
+                    distance = hit.distance;
+                }
+            }
+
+            transform.SetPositionAndRotation(pivot + rotation * Vector3.back * distance, rotation);
+        }
+    }
+}
+#endif
