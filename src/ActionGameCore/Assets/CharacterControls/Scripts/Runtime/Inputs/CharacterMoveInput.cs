e4be726ec19507d03d08ce325c20f9d8e4f59654
DAIKI <daiki.evilone@gmail.com>
2024-03-04 01:47:19 +0900

    Fix error with default input action asset

--- src/ActionGameCore/Assets/CharacterControls/Scripts/Runtime/Inputs/CharacterMoveInput.cs
+++ src/ActionGameCore/Assets/CharacterControls/Scripts/Runtime/Inputs/CharacterMoveInput.cs
@@ -1,6 +1,7 @@
 #if SUPPORT_INPUTSYSTEM
 using CharacterControls.Movements;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
 namespace CharacterControls.Inputs
@@ -10,6 +11,15 @@ namespace CharacterControls.Inputs
         [SerializeField]
         private InputActionAsset _inputActionAsset;
 
+        [SerializeField]
+        private string _actionMapName = "Player";
+
+        [SerializeField]
+        private string _moveActionName = "Move";
+
+        [SerializeField]
+        private string _jumpActionName = "Jump";
+
         public IMoveController MoveController { get; set; }
 
         private InputAction _moveAction;
@@ -18,24 +28,37 @@ namespace CharacterControls.Inputs
 
         private void Start()
         {
-            _inputActionAsset.Enable();
-            var map = _inputActionAsset.FindActionMap("Player");
-            _moveAction = map.FindAction("Move");
-            _jumpAction = map.FindAction("Jump");
+            Assert.IsNotNull(_inputActionAsset, "InputActionAsset is not set");
+            if (_inputActionAsset != null)
+            {
+                _inputActionAsset.Enable();
+                var map = _inputActionAsset.FindActionMap(_actionMapName);
+                Assert.IsNotNull(map, $"Action map {_actionMapName} not found");
+                if (map != null)
+                {
+                    _moveAction = map.FindAction(_moveActionName);
+                    _jumpAction = map.FindAction(_jumpActionName);
+                    Assert.IsNotNull(_moveAction, $"Action {_moveActionName} not found in {_actionMapName}");
+                    Assert.IsNotNull(_jumpAction, $"Action {_jumpActionName} not found in {_actionMapName}");
+                }
+            }
 
             MoveController = GetComponent<IMoveController>();
         }
 
         private void Update()
         {
-            if (_inputActionAsset == null || MoveController == null)
+            if (MoveController == null)
             {
                 return;
             }
 
-            MoveController.SetMoveInput(_moveAction.ReadValue<Vector2>());
+            if (_moveAction != null)
+            {
+                MoveController.SetMoveInput(_moveAction.ReadValue<Vector2>());
+            }
 
-            if (_jumpAction.WasPressedThisFrame())
+            if (_jumpAction != null && _jumpAction.WasPressedThisFrame())
             {
                 MoveController.SetJumpInput(1.0f);
             }
